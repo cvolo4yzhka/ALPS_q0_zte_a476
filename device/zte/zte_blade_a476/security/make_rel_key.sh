ea1ec7d746599d68150fa0221d0d8e761e6e5875
cvolo4yzhka <cvolo4yzhka@mail.ru>
2020-06-24 11:14:21 +0300

    script for gen rel key

--- device/zte/zte_blade_a476/security/make_rel_key.sh
+++ device/zte/zte_blade_a476/security/make_rel_key.sh
@@ -1,7 +1,9 @@
-in root of source
+#!/bin/bash
+cd ../../../..
 
 subject='/C=RU/ST=Tatarstan/L=Almetevsk/O=cvolo4yzhka,Inc./OU=cvolo4yzhka Mobility/CN=cvolo4yzhka/emailAddress=cvolo4yzhka@gmail.com'
-
 for x in releasekey platform shared media networkstack testkey verity verity_key; do \
 ./development/tools/make_key ./device/zte/zte_blade_a476/security/$x "$subject"; \
 done
+
+cd ./device/zte/zte_blade_a476/security/
