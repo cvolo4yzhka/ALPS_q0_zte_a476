c09e30ccfe8c0ecf8b483bae96a1f7419afea007
cvolo4yzhka <cvolo4yzhka@mail.ru>
2020-10-07 09:20:04 +0300

    some small changes

--- x32_compile_and.sh
+++ x32_compile_and.sh
@@ -3,7 +3,7 @@
 #export CCACHE_DIR=/home/cvolo4yzhka/.ccache
 #export CCACHE_EXEC=/usr/bin/ccache
 #ccache -M 50G
-#export _JAVA_OPTIONS="-Xmx6G"
+export _JAVA_OPTIONS="-Xmx6G"
 export WITHOUT_CHECK_API=true
 source build/envsetup.sh
 lunch full_zte_blade_a476-user
